9243ee51b66ff4d9dde7e952f92276304ae7e18b
brk-a <fnjakai@outlook.com>
2023-09-02 21:40:37 +0300

    body: wireframe half-done

--- src/components/Dash.jsx
+++ src/components/Dash.jsx
@@ -1,18 +1,42 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import profile from '../assets/profile.jpg'
+import emoji from "../assets/emoji.png"
+import imageBox from "../assets/imageBox.png"
+import locationPin from "../assets/locationPin.png"
+import poll from "../assets/poll.png"
+import video from "../assets/video.png"
+import addMember from "../assets/addMember.png"
+import setting from "../assets/setting.png"
+import tree from "../assets/tree.png"
+import PostCard from './PostCard'
+import Timeline from './Timeline'
+
+const icons = [
+    imageBox,
+    video,
+    poll,
+    emoji,
+    locationPin,
+]
+
+const treeActions = [
+    addMember,
+    setting,
+]
 
 const Dash = () => {
   return (
     <div className="flex flex-row gap-2 items-center">
         <div className="basis-2/5 h-full sm:mx-auto sm:w-full sm:max-w-sm">
             <div className='flex flex-row'>
-                <Image src={profile} width={24} height={24} className='rounded-full'/>
-                <div className='flex flex-row rounded-full'>
+                <Image src={profile} alt='pro-pic' width={24} height={24} className='rounded-full'/>
+                <div className='flex flex-row rounded-full justify-between items-center'>
                     <input type='text' placeholder="What's new?" className='basis-1/2 rounded-full'/>
-                    <div className='flex flex-row items-center justify-between'>
+                    <div className='flex flex-row items-center justify-between basis-1/2'>
                         {icons.map((icon, i) => (
                             <Link key={i} href="">
-                                <Image src={icon} width={24} height={24}/>
+                                <Image src={icon} width={24} height={24} alt={icon}/>
                             </Link>
                         ))}
                     </div>
@@ -22,26 +46,26 @@ const Dash = () => {
             {
                 posts.map((post, i) => (
                     <div key={i}>
-                        <PostCard/>
+                        <PostCard profilePic={post.profilePic} name={post.name} relationship={post.relationship} postedAt={post.postedAt} photo={post.photo}/>
                     </div>
                 ))
             }
             </>
         </div>
-        <div className="basis-3/5">
-            <div className='flex flex-col rounded-md'>
+        <div className="basis-3/5 flex-row gap-1">
+            <div className={`flex flex-col rounded-md tree`}>
                 <div className='flex flex-row justify-between'>
                     {treeActions.map((treeAction, i) => (
                         <Link href="" key={i}>
-                            <Image src={treeAction} width={24} height={24}/>
+                            <Image src={treeAction} width={24} height={24} alt={tree-action}/>
                         </Link>
                     ))}
                 </div>
                 <div>
-                    <Image src={tree} width={600} height={450}/>
+                    <Image src={tree} width={600} height={450} alt='tree'/>
                 </div>
             </div>
-            <div>
+            <div className={`flex flex-col rounded-md tree`}>
                 <Timeline/>
             </div>
         </div>
--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import Image from 'next/image'
+import Reaction from './Reaction'
+import Comment from './Comment'
 
-const PostCard = () => {
+const PostCard = ({profilePic, name, relationship, postedAt, photo}) => {
   return (
-    <div>PostCard</div>
+    <div className={`flex flex-col gap-3 items-center justify-center w-[522px] h-[605px] rounded-md post-card`}>
+      <div className='flex flex-row'>
+        <Image src={profilePic} width={24} height={24} alt='pro-pic'/>
+        <div className='flex flex-col'>
+          <div className='flex flex-row'>
+            <h2>{name}</h2>
+            <p>{postedAt}</p>
+          </div>
+          <p>{relationship}</p>
+        </div>
+        <Image src={option} alt='option' width={23} height={28} className='flex-shrink-0'/>
+      </div>
+      <div className='flex flex-col rounded-md bg-white w-[481px] h-[499px] flex-shrink-0'>
+        <p>
+          {message}
+        </p>
+        <Image src={photo} alt='option' width={430} height={255} className='flex-shrink-0 rounded-sm'/>
+      </div>
+      <div>
+        <Reaction/>
+      </div>
+      <div>
+        <Comment/>
+      </div>
+    </div>
   )
 }
 
