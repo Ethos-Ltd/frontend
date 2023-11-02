dd846b5115689875f7a950e17e7e346f3af8e860
brk-a <fnjakai@outlook.com>
2023-11-02 19:26:49 +0300

    add timeline

---
+++ src/app/dump/timelineItems.js
@@ -0,0 +1,5 @@
+const timelineItems = [
+    {},
+]
+
+export default timelineItems
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,8 +1,12 @@
+import timelineItems from '@/app/dump/timelineItems'
 import React from 'react'
+import { Chrono } from 'react-chrono'
 
 const Timeline = () => {
   return (
-    <div>Timeline</div>
+    <div style={{width: 600, height: 500}}>
+      <Chrono items={timelineItems}/>
+    </div>
   )
 }
 
